95d2b57e646289d250c1b852a5d9a2dd56ff4d57
Bikkul <quadalus@yandex.ru>
2023-01-21 16:14:35 +0300

    feat: add model

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.*;
+import ru.practicum.shareit.request.ItemRequest;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
 public class Item {
+    private Long id;
+    private String name;
+    private String description;
+    private ItemStatus available;
+    private String owner;
+    private ItemRequest request;
 }
--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.user;
 
+import lombok.*;
+
+import javax.validation.constraints.Email;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString
 public class User {
+    private Long id;
+    private String name;
+    @Email
+    private String email;
 }
