07ee6d8781dde8d0ed1e0c34c8a4fa512ce54ca1
Bikkul <quadalus@yandex.ru>
2023-01-27 19:21:52 +0300

    fix: add whitespaces

--- src/main/java/ru/practicum/shareit/exception/IncorrectParameterException.java
+++ src/main/java/ru/practicum/shareit/exception/IncorrectParameterException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class IncorrectParameterException extends RuntimeException{
+public class IncorrectParameterException extends RuntimeException {
     public IncorrectParameterException(String message) {
         super(message);
     }
