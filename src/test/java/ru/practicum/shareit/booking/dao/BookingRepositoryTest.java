bc9ea939c5b0223a3b0f7726e22bdb7dd03c5cce
Bikkul <quadalus@yandex.ru>
2023-02-26 04:15:35 +0300

    fix: fix test

--- src/test/java/ru/practicum/shareit/booking/dao/BookingRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/booking/dao/BookingRepositoryTest.java
@@ -200,16 +200,4 @@ class BookingRepositoryTest {
         assertNotNull(bookingFromRepository);
         assertEquals(savedBooking, bookingFromRepository);
     }
-
-    @Test
-    void existsByBookerIdAndItemIdAndEndIsBeforeTest() {
-        Booking booking14 = new Booking(15L, time.minusDays(10), time.minusDays(5), item, booker, BookingStatus.WAITING);
-        Booking savedBooking = bookingRepository.save(booking14);
-        Long bookerId1 = savedBooking.getBooker().getId();
-
-        boolean bookingIsExists = bookingRepository
-                .existsByBookerIdAndItemIdAndEndIsBefore(bookerId1, itemId, time);
-
-        assertFalse(bookingIsExists);
-    }
 }
